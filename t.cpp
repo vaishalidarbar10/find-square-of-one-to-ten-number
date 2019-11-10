40cd6cf346f26120f7c62b879ddddb4f07937c5c
vaishalidarbar10 <57501404+vaishalidarbar10@users.noreply.github.com>
2019-11-10 22:56:35 +0530

    Create t.cpp

---
+++ t.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int i,sqr;
+    while(i<11)
+    {
+        sqr = i*i;
+        cout<<"Square of "<<i<<" is "<<sqr<<endl;
+        i++;
+    }
+    getch();
+    
